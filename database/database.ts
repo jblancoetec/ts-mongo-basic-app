47fea0b4f5f45be57449ca87d4b7e115e9ed24d6
Joaquin Blanco <jblanco@etec.uba.ar>
2022-06-17 03:47:36 -0300

    add types Employee and EmployeeWithID

--- database/database.ts
+++ database/database.ts
@@ -1,24 +1,26 @@
 import { MongoClient } from "mongodb";
 
+export type Employee = {
+  name: string;
+  salary: number;
+};
+
+export type EmployeeWithID = Employee & {
+  _id: string;
+};
+
+const employees: Employee[] = [
+  { name: "Ryan Ray", salary: 50000 },
+  { name: "John Doe", salary: 100000 },
+  { name: "Jane Doe", salary: 200000 },
+];
+
 export const createDB = async (client: MongoClient) => {
   try {
     await client.connect();
     const db = client.db("company");
     const collection = db.collection("employees");
-    await collection.insertMany([
-      {
-        name: "Ryan Ray",
-        salary: 20000,
-      },
-      {
-        name: "John McMillan",
-        salary: 40000,
-      },
-      {
-        name: "John Carter",
-        salary: 50000,
-      },
-    ]);
+    await collection.insertMany(employees);
   } catch (err) {
     console.log(err);
   }
--- src/DBCompany.ts
+++ src/DBCompany.ts
@@ -1,30 +1,17 @@
 import { Db } from "mongodb";
-
-export type Employee = {
-  _id: string;
-  name: string;
-  salary: number;
-};
+import { Employee } from "../database/database";
 
 class DBCompany {
   constructor(private readonly db: Db) {}
 
   async getEmployees(): Promise<Employee[]> {
-    try {
-      const collection = this.db.collection("employees");
-      const documents = await collection.find({}).toArray();
-      const employees: Employee[] = documents.map((document) => {
-        return {
-          _id: document._id.toString(),
-          name: document.name,
-          salary: document.salary,
-        };
-      });
-      return employees;
-    } catch {
-      console.log("Error al obtener los empleados");
-      return [];
-    }
+    const collection = this.db.collection("employees");
+    const documents = await collection.find({}).toArray();
+    const employees: Employee[] = documents.map((document) => ({
+      name: document.name,
+      salary: document.salary,
+    }));
+    return employees;
   }
 }
 
