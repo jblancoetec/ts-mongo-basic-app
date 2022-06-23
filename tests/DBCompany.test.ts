e3701e8be34558dd4318ea274ca9023c03638ea4
Joaquin Blanco <jblanco@etec.uba.ar>
2022-06-23 01:32:10 -0300

    update reports error

--- tests/DBCompany.test.ts
+++ tests/DBCompany.test.ts
@@ -13,19 +13,21 @@ afterAll(() => {
   return destroyDB();
 });
 
-test("Deberian existir tres empleados en la base de datos", async () => {
-  const employees = await db.getEmployees();
-  expect(employees.length).toBe(3);
-});
+describe("Test DBCompany", () => {
+  test("Deberian existir tres empleados en la base de datos", async () => {
+    const employees = await db.getEmployees();
+    expect(employees.length).toBe(3);
+  });
 
-test("Deberia existir un empleado con sueldo igual a 50000", async () => {
-  const employees = await db.getEmployees();
-  const employee = employees.find((employee) => employee.salary === 50000);
-  expect(employee).toBeDefined();
-});
+  test("Deberia existir un empleado con sueldo igual a 50000", async () => {
+    const employees = await db.getEmployees();
+    const employee = employees.find((employee) => employee.salary === 50000);
+    expect(employee).toBeDefined();
+  });
 
-test("Deberia existir un empleado llamado Ryan Ray", async () => {
-  const employees = await db.getEmployees();
-  const employee = employees.find((employee) => employee.name === "Ryan Ray");
-  expect(employee).toBeDefined();
+  test("Deberia existir un empleado llamado Ryan Ray", async () => {
+    const employees = await db.getEmployees();
+    const employee = employees.find((employee) => employee.name === "Ryan Ray");
+    expect(employee).toBeDefined();
+  });
 });
